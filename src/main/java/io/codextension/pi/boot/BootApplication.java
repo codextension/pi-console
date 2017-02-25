01aae1a148cdad4b643b630974c21eb9b29f6a12
elie khoury <elie@el-khoury.net>
2017-02-25 12:29:22 +0100

    test

--- src/main/java/io/codextension/pi/boot/BootApplication.java
+++ src/main/java/io/codextension/pi/boot/BootApplication.java
@@ -1,14 +1,12 @@
 package io.codextension.pi.boot;
 
+import com.pi4j.io.gpio.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-
-import java.util.Arrays;
 
 /**
  * Created by eelkhour on 24.02.2017.
@@ -24,14 +22,11 @@ public class BootApplication {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
+            final GpioController gpio = GpioFactory.getInstance();
+            GpioPinDigitalOutput myLed = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_17,   // PIN NUMBER
+                                                                       "My LED",           // PIN FRIENDLY NAME (optional)
+                                                                       PinState.LOW);      // PIN STARTUP STATE (optional)
+            myLed.high();
         };
     }
 }
