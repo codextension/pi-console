d025fead2a916a71efbbae38ce56078f2fb80a17
elie khoury <elie@el-khoury.net>
2017-02-25 20:57:55 +0100

    test

--- src/main/java/io/codextension/pi/controller/LightController.java
+++ src/main/java/io/codextension/pi/controller/LightController.java
@@ -10,30 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 /**
  * Created by eelkhour on 24.02.2017.
  */
 @RestController
 @RequestMapping("/lights")
 public class LightController {
-    GpioController gpio;
-
-    @PostConstruct
-    public void init() {
-       gpio = GpioFactory.getInstance();
-    }
-
-    @PreDestroy
-    public void destroy() {
-        gpio.shutdown();
-    }
 
     @RequestMapping(value = "/{state}", method = RequestMethod.GET)
     public String switchLights(@PathVariable(required = true, name = "state") String state, Model model) {
-        GpioPinDigitalOutput myLed_1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_11, "My LED 1");
+        GpioController gpio = GpioFactory.getInstance();
+        GpioPinDigitalOutput myLed_1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_12, "My LED 1");
         GpioPinDigitalOutput myLed_2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "My LED 2");
 
         if (state.equalsIgnoreCase("off")) {
@@ -43,7 +30,7 @@ public class LightController {
             myLed_1.high();
             myLed_2.high();
         }
-
+        gpio.shutdown();
         return "Setting light state to: " + state;
     }
 
