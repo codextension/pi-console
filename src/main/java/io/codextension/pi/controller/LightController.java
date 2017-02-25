4c8773f6f80171a6203790bc3493712aa3ed2213
elie khoury <elie@el-khoury.net>
2017-02-25 20:21:44 +0100

    test

--- src/main/java/io/codextension/pi/controller/LightController.java
+++ src/main/java/io/codextension/pi/controller/LightController.java
@@ -5,20 +5,25 @@ import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.RaspiPin;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 /**
  * Created by eelkhour on 24.02.2017.
  */
 @RestController
+@RequestMapping("/lights")
 public class LightController {
     GpioController gpio;
 
-    @PostMapping
+    @PostConstruct
     public void init() {
-        gpio = GpioFactory.getInstance();
+       gpio = GpioFactory.getInstance();
     }
 
     @PreDestroy
@@ -26,8 +31,8 @@ public class LightController {
         gpio.shutdown();
     }
 
-    @RequestMapping(value = "/lights/{state}", method = RequestMethod.GET)
-    public String switchLights(@PathVariable(value = "off", required = true, name = "state") String state, Model model) {
+    @RequestMapping(value = "/{state}", method = RequestMethod.GET)
+    public String switchLights(@PathVariable(required = true, name = "state") String state, Model model) {
         GpioPinDigitalOutput myLed_1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "My LED 1");
         GpioPinDigitalOutput myLed_2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "My LED 2");
 
