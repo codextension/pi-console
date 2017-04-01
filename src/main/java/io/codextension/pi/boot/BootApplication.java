feee83e7738323417080f8f7dc6a0222fc561ad0
Elie Khoury <elie@el-khoury.net>
2017-04-01 12:00:37 +0200

    enable gpio globally

--- src/main/java/io/codextension/pi/boot/BootApplication.java
+++ src/main/java/io/codextension/pi/boot/BootApplication.java
@@ -1,12 +1,16 @@
 package io.codextension.pi.boot;
 
 import com.pi4j.wiringpi.Gpio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by eelkhour on 24.02.2017.
@@ -15,13 +19,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableMongoRepositories(basePackages = {"io.codextension.pi.model", "io.codextension.pi.repository"})
 @EnableScheduling
 @ComponentScan(basePackages = "io.codextension.pi")
-public class BootApplication implements CommandLineRunner {
+public class BootApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BootApplication.class);
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(BootApplication.class, args);
     }
 
-    @Override
-    public void run(String... strings) throws Exception {
+    @PostConstruct
+    public void init() {
+        LOG.info("Enabling GPIO ...");
         Gpio.wiringPiSetupGpio();
     }
 }
