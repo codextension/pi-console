82c997a49a95f17d6027839b8f95bcb8f0c55e75
Elie Khoury <elie@el-khoury.net>
2017-04-14 22:48:04 +0200

    enable gpio globally

--- src/main/java/io/codextension/pi/controller/DustSensorController.java
+++ src/main/java/io/codextension/pi/controller/DustSensorController.java
@@ -38,7 +38,7 @@ public class DustSensorController {
 
         GpioUtil.unexport(12);
         provider.unexport(MCP3008Pin.CH0);
-        return (inValue * (5 / 1024)) * 0.17 - 0.1;
+        return ((inValue * (3.0 / 1024)) * 0.17 - 0.1) * 1000;
 
     }
 }
