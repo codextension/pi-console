7c261fe4d73c072513bb685913f542058bdc65d4
Elie Khoury <elie@el-khoury.net>
2017-04-16 13:58:08 +0200

    improving the dust sensor

--- src/main/java/io/codextension/pi/controller/DustSensorController.java
+++ src/main/java/io/codextension/pi/controller/DustSensorController.java
@@ -15,12 +15,12 @@ import io.codextension.pi.repository.DustSensorRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -35,6 +35,13 @@ public class DustSensorController {
 
     @Autowired
     private DustSensorRepository dustSensorRepository;
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy-hh:mm");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(
+                dateFormat, true));
+    }
 
     @RequestMapping("/current")
     public double getCurrent() throws InterruptedException, IOException {
