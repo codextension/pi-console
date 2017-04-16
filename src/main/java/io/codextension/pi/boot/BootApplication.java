344d3480948e1681165ce6f23fb0ff2a6197127c
Elie Khoury <elie@el-khoury.net>
2017-04-16 18:22:21 +0200

    polling every 15sec

--- src/main/java/io/codextension/pi/boot/BootApplication.java
+++ src/main/java/io/codextension/pi/boot/BootApplication.java
@@ -6,11 +6,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.Executor;
 
 /**
  * Created by eelkhour on 24.02.2017.
@@ -18,12 +24,24 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = {"io.codextension.pi.model", "io.codextension.pi.repository"})
 @EnableScheduling
+@EnableAsync
 @ComponentScan(basePackages = "io.codextension.pi")
 public class BootApplication {
     private static final Logger LOG = LoggerFactory.getLogger(BootApplication.class);
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(BootApplication.class, args);
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ConcurrentTaskScheduler();
+    }
+
+    // Of course , you can define the Executor too
+    @Bean
+    public Executor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
     }
 
     @PostConstruct
