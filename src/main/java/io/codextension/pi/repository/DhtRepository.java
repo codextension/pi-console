3a0a8be1c2d3f02f3884e934e3d59d66fc04c160
Elie Khoury <elie@el-khoury.net>
2017-04-22 06:07:39 +0200

    switched to h2

--- src/main/java/io/codextension/pi/repository/DhtRepository.java
+++ src/main/java/io/codextension/pi/repository/DhtRepository.java
@@ -2,7 +2,6 @@ package io.codextension.pi.repository;
 
 import io.codextension.pi.model.Dht;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
@@ -10,8 +9,7 @@ import java.util.List;
 /**
  * Created by elie on 09.03.17.
  */
-@Repository
-public interface DhtRepository extends CrudRepository<Dht, String> {
+public interface DhtRepository extends CrudRepository<Dht, Long> {
 
     List<Dht> findByMeasuredDateBetweenOrderByMeasuredDateDesc(Date from, Date to);
 }
--- src/main/java/io/codextension/pi/repository/DustSensorRepository.java
+++ src/main/java/io/codextension/pi/repository/DustSensorRepository.java
@@ -2,7 +2,6 @@ package io.codextension.pi.repository;
 
 import io.codextension.pi.model.Dust;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
@@ -10,8 +9,7 @@ import java.util.List;
 /**
  * Created by elie on 16.04.17.
  */
-@Repository
-public interface DustSensorRepository extends CrudRepository<Dust, String> {
+public interface DustSensorRepository extends CrudRepository<Dust, Long> {
     List<Dust> findByMeasuredDateBetweenOrderByMeasuredDateDesc(Date from, Date to);
 
 }
