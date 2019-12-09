2c99e32716c3d532730edb56a66e2ad23c10bba8
Elie Khoury <elie@el-khoury.net>
2019-12-09 20:50:50 +0100

    change structure

--- py-api/process_sensor_data.py
+++ py-api/process_sensor_data.py
@@ -6,11 +6,18 @@ from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
 if __name__ == "__main__":
+
+    def avg_map_func(row):
+        return (row[0], (row[2], 1))
+
+    def avg_reduce_func(value1, value2):
+        return ((value1[0] + value2[0], value1[1] + value2[1])) 
+
     sc = SparkContext(appName="TestApp")
     ssc = StreamingContext(sc, 60)
     kvs = KafkaUtils.createDirectStream(ssc, ["temperature"], {"metadata.broker.list":"localhost:9092"})
-    
-    avg_by_key = kvs.mapValues(lambda v: (v, 1)).reduceByKey(lambda a,b: (a[0]+b[0], a[1]+b[1])).mapValues(lambda v: v[0]/v[1])
+
+    avg_by_key = kvs.map(avg_map_func).reduceByKey(avg_reduce_func).mapValues(lambda x: x[0]/x[1])
     avg_by_key.pprint()
 
     ssc.start()
