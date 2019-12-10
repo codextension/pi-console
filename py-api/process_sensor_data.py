1c25273200de137ae8407aeed949dbf2e157ceb3
Elie Khoury <elie@el-khoury.net>
2019-12-10 05:28:44 +0100

    key value serilaieer

--- py-api/process_sensor_data.py
+++ py-api/process_sensor_data.py
@@ -4,20 +4,15 @@ import sys
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+import json
 
 if __name__ == "__main__":
 
-    def avg_map_func(row):
-        return (row[0], (row[2], 1))
-
-    def avg_reduce_func(value1, value2):
-        return ((value1[0] + value2[0], value1[1] + value2[1])) 
-
     sc = SparkContext(appName="TestApp")
     ssc = StreamingContext(sc, 60)
-    kvs = KafkaUtils.createDirectStream(ssc, ["dust"], {"metadata.broker.list":"localhost:9092"})
+    kvs = KafkaUtils.createDirectStream(ssc, ["dust"], {"metadata.broker.list":"localhost:9092"}, valueDecoder=lambda m: json.loads(m.decode('utf-8')))
 
-    avg_by_key = kvs.map(avg_map_func).reduceByKey(avg_reduce_func).mapValues(lambda x: x[0]/x[1])
+    avg_by_key = kvs.groupByKey()
     avg_by_key.pprint()
 
     ssc.start()
