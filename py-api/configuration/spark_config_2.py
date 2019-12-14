e879bb75d166a0c413d783ee3810932ae66eeb28
Elie Khoury <elie@el-khoury.net>
2019-12-14 12:18:53 +0100

    now working!

--- py-api/configuration/spark_config_2.py
+++ py-api/configuration/spark_config_2.py
@@ -9,13 +9,15 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
 if __name__ == "__main__":
+    temp_schema = StructType([
+            StructField("temperature", StringType(), True),
+            StructField("humidity", StringType(), True),
+            StructField("timestamp", StringType(), True)])
+
     spark = SparkSession.builder.appName("StructuredNetworkWordCount").getOrCreate()
-    df = spark.readStream.format('kafka').option("kafka.bootstrap.servers", "192.168.178.63:9092").option("subscribe", "temperature").option("kafkaConsumer.pollTimeoutMs", "30000").option("startingOffsets", "latest").load()
-    df.printSchema()
-    
-    temp_schema = StructType().add("temperature","float").add("humidity", "float").add("timestamp", "timestamp")
+    df = spark.readStream.format('kafka').option("kafka.bootstrap.servers", "192.168.178.63:9092").option("subscribe", "temperature").option("startingOffsets", "earliest").load()
 
     sensor = df.withColumn("value", df.value.astype('string'))
-    sensor = sensor.withWatermark("timestamp", "10 minutes").groupBy(window("timestamp", "2 minutes", "1 minute"), "value").count()
-    sensor =sensor.select(from_json("value", temp_schema).alias("data")).select("data.*")
+    sensor = sensor.withWatermark("timestamp", "10 minutes").groupBy(window("timestamp", "20 seconds", "10 seconds"), "value").count()
+    sensor =sensor.select(from_json(sensor.value, temp_schema).alias("data")).select("data.*")
     sensor.writeStream.format("console").outputMode("append").start().awaitTermination()
