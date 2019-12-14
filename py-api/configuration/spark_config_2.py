9418f631d3ffa58d423cffe88df3ac1c9fa9360e
Elie Khoury <elie@el-khoury.net>
2019-12-14 18:13:32 +0100

    reading dust

--- py-api/configuration/spark_config_2.py
+++ py-api/configuration/spark_config_2.py
@@ -12,24 +12,27 @@ from pyspark.sql.functions import *
 if __name__ == "__main__":
     temp_schema = StructType(
         [
-            StructField("value", StringType(), True),
-            StructField("voltage", StringType(), True),
-            StructField("density", StringType() , True)
+            StructField("value", IntegerType(), True),
+            StructField("voltage", FloatType(), True),
+            StructField("density", DoubleType() , True)
         ]
     )
 
-    spark = SparkSession.builder.appName("noise_parser").getOrCreate()
+    spark = SparkSession.builder.appName("dust_parser").getOrCreate()
     df = (
         spark.readStream.format("kafka")
         .option("kafka.bootstrap.servers", "192.168.178.63:9092")
-        .option("subscribe", "noise")
+        .option("subscribe", "dust")
         .option("startingOffsets", "earliest")
         .load()
     )
 
     sensor = df.withColumn("value", df.value.astype("string"))
-    
-
+    sensor = (
+        sensor.withWatermark("timestamp", "5 minutes")
+        .groupBy(window("timestamp", "2 minutes", "1 minutes"), "value")
+        .count()
+    )
 
     sensor = sensor.select(from_json(sensor.value, temp_schema).alias("data")).select(
         "data.*"
