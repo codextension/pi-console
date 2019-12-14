3ae5b153e5b3c240723f9ebdf7656c3b2f56b6e7
Elie Khoury <elie@el-khoury.net>
2019-12-14 11:32:53 +0100

    now working

--- py-api/configuration/spark_config_2.py
+++ py-api/configuration/spark_config_2.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     df = spark.readStream.format('kafka').option("kafka.bootstrap.servers", "192.168.178.63:9092").option("subscribe", "temperature").option("startingOffsets", "earliest").load()
     df.printSchema()
     
-    temp_schema = StructType().add("temperature","float").add("humidity", "float").add("timestamp", "datetime")
+    temp_schema = StructType().add("temperature","float").add("humidity", "float").add("timestamp", "timestamp")
 
     sensor = df.withColumn("value", df.value.astype('string'))
-    sensor =sensor.select(from_json(sensor.value, temp_schema).alias("data")).select("data.*")
-    sensor = sensor.groupBy(window(df.timestamp, "20 seconds", "10 seconds"),df.value).count()
+    sensor = sensor.withWatermark("timestamp", "10 minutes").groupBy(window("timestamp", "2 minutes", "1 minute"), "value").count()
+    sensor =sensor.select(from_json("value", temp_schema).alias("data")).select("data.*")
     sensor.writeStream.format("console").outputMode("append").start().awaitTermination()
