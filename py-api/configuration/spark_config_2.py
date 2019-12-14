3fcc06b3aefe5bb7cb46462fcc08b1391d483ab1
Elie Khoury <elie@el-khoury.net>
2019-12-14 10:56:04 +0100

    added timestamp

--- py-api/configuration/spark_config_2.py
+++ py-api/configuration/spark_config_2.py
@@ -10,9 +10,12 @@ from pyspark.sql.functions import *
 
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("StructuredNetworkWordCount").getOrCreate()
-    df = spark.readStream.format('kafka').option("kafka.bootstrap.servers", "192.168.178.63:9092").option("value.deserializer","lambda m: json.loads(m.decode('utf-8'))").option("subscribe", "temperature").load()
-    df.selectExpr("CAST(key AS STRING) AS key", "to_json(struct(*)) as value")
+    df = spark.readStream.format('kafka').option("kafka.bootstrap.servers", "192.168.178.63:9092").option("subscribe", "temperature").option("startingOffsets", "earliest").load()
     df.printSchema()
+    
+    temp_schema = StructType().add("temperature","float").add("humidity", "float").add("timestamp", "datetime")
 
-    df.writeStream.format("console").option(
-        "truncate", "false").start().awaitTermination()
+    sensor = df.withColumn("value", df.value.astype('string'))
+    sensor =sensor.select(from_json(sensor.value, temp_schema).alias("data")).select("data.*")
+    sensor = sensor.groupBy(window(df.timestamp, "20 seconds", "10 seconds"),df.value).count()
+    sensor.writeStream.format("console").outputMode("append").start().awaitTermination()
